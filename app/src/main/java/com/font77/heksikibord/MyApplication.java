4961f3c525efa5175f3c84446d41f7b1efccb579
Font77 <vimalkg@gmail.com>
2020-05-17 20:25:53 +0530

    vk.com/etphor

--- app/src/main/java/com/font77/heksikibord/MyApplication.java
+++ app/src/main/java/com/font77/heksikibord/MyApplication.java
@@ -5,8 +5,8 @@ public final class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        fontsovArride.setDefaultFont(this, "DEFAULT", R.font.roboto_binjk_horijntl);
-//        fontsovArride.setDefaultFont(this, "DEFAULT", R.font.roboto_heks_jk);
+//        fontsovArride.setDefaultFont(this, "DEFAULT", R.font.roboto_binjk_horijntl);
+        fontsovArride.setDefaultFont(this, "DEFAULT", R.font.roboto_heks_zk);
         fontsovArride.setDefaultFont(this, "MONOSPACE", R.font.ddssmonodc);
     }
 }
