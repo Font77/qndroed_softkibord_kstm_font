0437e14e14351b09d499d51b4d870023070c2f8a
Font77 <vimalkg@gmail.com>
2020-05-29 05:00:58 +0530

    vk.com/etphor

--- app/src/main/java/com/font77/heksikibord/lAtinqibordviyu.java
+++ app/src/main/java/com/font77/heksikibord/lAtinqibordviyu.java
@@ -1,29 +1,18 @@
 package com.font77.heksikibord;
-
 import android.content.Context;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.util.AttributeSet;
 import android.view.inputmethod.InputMethodSubtype;
-
 public class lAtinqibordviyu extends KeyboardView {
     static final int KEYCODE_OPTIONS = -100;
-    // TODO: Move this into android.inputmethodservice.Keyboard
     static final int KEYCODE_LANGUAGE_SWITCH = -101;
-    public lAtinqibordviyu(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-    public lAtinqibordviyu(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+    public lAtinqibordviyu(Context context, AttributeSet attrs) { super(context, attrs); }
+    public lAtinqibordviyu(Context context, AttributeSet attrs, int defStyle) { super(context, attrs, defStyle); }
     @Override
     protected boolean onLongPress(Keyboard.Key key) {
-        if (key.codes[0] == Keyboard.KEYCODE_CANCEL) {
-            getOnKeyboardActionListener().onKey(KEYCODE_OPTIONS, null);
-            return true;
-        } else {
-            return super.onLongPress(key);
-        }
+        if (key.codes[0] == Keyboard.KEYCODE_CANCEL) { getOnKeyboardActionListener().onKey(KEYCODE_OPTIONS, null); return true; }
+        else { return super.onLongPress(key); }
     }
     void setSubtypeOnSpaceKey(final InputMethodSubtype subtype) {
         final lAtinqibord keyboard = (lAtinqibord)getKeyboard();
